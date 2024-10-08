b90eea31a42b8db98e891e01842525e6fb065776
altos721 <altos721@gmail.com>
2024-10-08 14:16:20 +0800

    Change bmi to struct with color property

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,16 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Chihhao Hsu on 2024/10/8.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+}
